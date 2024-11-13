c086878ef9999181d19100466c3e6187998d92cc
Maksim Botalov <dev.botalov@icloud.com>
2024-11-13 18:40:13 +0500

    Initial Commit

---
+++ DiffableCollectionContainer/DiffableCollectionContainer.h
@@ -0,0 +1,18 @@
+//
+//  DiffableCollectionContainer.h
+//  DiffableCollectionContainer
+//
+//  Created by Максим Боталов on 13.11.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DiffableCollectionContainer.
+FOUNDATION_EXPORT double DiffableCollectionContainerVersionNumber;
+
+//! Project version string for DiffableCollectionContainer.
+FOUNDATION_EXPORT const unsigned char DiffableCollectionContainerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DiffableCollectionContainer/PublicHeader.h>
+
+
